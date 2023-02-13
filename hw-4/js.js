e730d7a06646ab5b6747edbfcb627ff98b4f8473
nesteR10 <andrey.nesterenko10@gmail.com>
2023-02-13 16:27:22 +0200

    add hw4

---
+++ hw-4/js.js
@@ -0,0 +1,6 @@
+const num1 = +prompt('Enter first number');
+const num2 = +prompt('Enter second number');
+const num3 = +prompt('Enter third number');
+const res = (num1 + num2 + num3)/3;
+
+alert(`Mean value is: ${res}`);
